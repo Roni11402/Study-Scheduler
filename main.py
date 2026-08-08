7920b5a0ad7058c5a5aadebed282d5cdcd03ace5
Roni Nahum <roninahum13@gmail.com>
2026-08-08 12:36:36 +0300

    Created HeapTaskManager with add_task and heapify

--- main.py
+++ main.py
@@ -21,70 +21,93 @@ class Node:
         self.next = None
 
 
-class task_manager:
-    def __init__(self):
-        self.head = None
+# class task_manager:
+#     def __init__(self):
+#         self.head = None
 
-    def add_task(self, task):
-        new_node = Node(task)
-        if self.head is None or new_node.task.priority < self.head.task.priority:
-            new_node.next = self.head
-            self.head = new_node
-        else:
-            current = self.head
-            while current.next and current.next.task.priority <= new_node.task.priority:
-                current = current.next
-            new_node.next = current.next
-            current.next = new_node
+#     def add_task(self, task):
+#         new_node = Node(task)
+#         if self.head is None or new_node.task.priority < self.head.task.priority:
+#             new_node.next = self.head
+#             self.head = new_node
+#         else:
+#             current = self.head
+#             while current.next and current.next.task.priority <= new_node.task.priority:
+#                 current = current.next
+#             new_node.next = current.next
+#             current.next = new_node
 
-    def remove_task(self, title):
-         if self.head is None:
-             return
-         if self.head.task.title == title:
-            self.head = self.head.next
-            return
-         else:
-             current = self.head
-             while current.next:
-                 if current.next.task.title == title:
-                     current.next = current.next.next
-                     return
-                 else:
-                     current = current.next
+#     def remove_task(self, title):
+#          if self.head is None:
+#              return
+#          if self.head.task.title == title:
+#             self.head = self.head.next
+#             return
+#          else:
+#              current = self.head
+#              while current.next:
+#                  if current.next.task.title == title:
+#                      current.next = current.next.next
+#                      return
+#                  else:
+#                      current = current.next
                      
 
-    def print_all_tasks(self):
-        current = self.head
-        while current:
-            print(current.task)
-            current = current.next
+#     def print_all_tasks(self):
+#         current = self.head
+#         while current:
+#             print(current.task)
+#             current = current.next
 
-    def save_to_file(self, file_name):
-        with open(file_name + ".csv", "w") as file:
-            current = self.head
-            while current:
-                line_to_write = current.task.to_csv() + "\n"
-                file.write(line_to_write)
-                current = current.next
+#     def save_to_file(self, file_name):
+#         with open(file_name + ".csv", "w") as file:
+#             current = self.head
+#             while current:
+#                 line_to_write = current.task.to_csv() + "\n"
+#                 file.write(line_to_write)
+#                 current = current.next
 
-    def load_from_file(self, file_name):
-        with open(file_name + ".csv", "r") as file:
-            for line in file:
-                clean_text = line.strip()
-                data = clean_text.split(',')
-                new_task = Task(data[0], data[1], int(data[2]))
-                new_task.is_completed = (data[3] == "True")
-                self.add_task(new_task)
+#     def load_from_file(self, file_name):
+#         with open(file_name + ".csv", "r") as file:
+#             for line in file:
+#                 clean_text = line.strip()
+#                 data = clean_text.split(',')
+#                 new_task = Task(data[0], data[1], int(data[2]))
+#                 new_task.is_completed = (data[3] == "True")
+#                 self.add_task(new_task)
+
+class HeapTaskManager:
+    def __init__(self):
+        self.heap = []
+
+    def parent(self, index):
+        return (index-1)//2
+
+    def left_child(self,index):
+        return 2*index + 1
+
+    def right_child(self,index):
+        return 2*index + 2
+
+    def heapify(self, index):
+        while index > 0 and self.heap[index].priority < self.heap[self.parent(index)].priority:
+            p_index = self.parent(index)
+            self.heap[index], self.heap[p_index] = self.heap[p_index], self.heap[index]
+            index = p_index
+
+    def add_task(self, task):
+        self.heap.append(task)
+        self.heapify(len(self.heap) - 1)
                            
 
-manager = task_manager()
+# יצירת המנהל החדש
+manager = HeapTaskManager()
 
-print("--- Loading from file... ---")
-manager.load_from_file("my_tasks")
-manager.print_all_tasks()
+# הוספת משימות בסדר מבולגן (העדיפות היא המספר האחרון)
+manager.add_task(Task("Do Laundry", "2026-08-10", 5))
+manager.add_task(Task("Submit Algebra Paper", "2026-08-12", 1))
+manager.add_task(Task("Buy Groceries", "2026-08-09", 3))
 
-print("\n--- Adding a new task ---")
-manager.add_task(Task("Learn Python", "2026-03-01", 1))
-manager.save_to_file("my_tasks") 
-
-print("Saved! Now close the program and run it again.")
+# בואי נראה מי יושב עכשיו בראש הערימה (באינדקס 0)
+print("The most urgent task is:")
+print(manager.heap[0])
